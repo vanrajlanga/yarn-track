20f330c5c6405ffa1e5a74762ad49a0aff67e71f
Chintan Gohil <echintangohil@gmail.com>
2025-05-26 14:27:36 +0530

    change for label

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -134,19 +134,6 @@ export const LoginPage = () => {
 							</Button>
 						</div>
 					</form>
-
-					<div className="mt-6">
-						<div className="relative">
-							<div className="absolute inset-0 flex items-center">
-								<div className="w-full border-t border-gray-300"></div>
-							</div>
-							<div className="relative flex justify-center text-sm">
-								<span className="px-2 bg-white text-gray-500">
-									Test Login Credentials
-								</span>
-							</div>
-						</div>
-					</div>
 				</div>
 			</div>
 		</div>
