b2c81005a37a9054f5a3d677fdec8c58ab488d1f
Chintan Gohil <echintangohil@gmail.com>
2025-05-26 16:36:46 +0530

    everything in Upper case

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.addEventListener('input', (event) => {
+    const target = event.target;
+    if (target.tagName === 'INPUT' && target.type === 'text' || target.tagName === 'TEXTAREA') {
+      target.value = target.value.toUpperCase();
+    }
+  });
+});
+
 createRoot(document.getElementById("root")).render(<App />);
